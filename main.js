89d9f9f8e490e1f177c824e6add59ecfd4211b7a
udaymuthuswamy <96528541+udaymuthuswamy@users.noreply.github.com>
2021-12-22 22:23:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+screen_width= 0;
+screen_height= 0;
+
+speak_data="";
+
+to_number=0;
+
+function preload()
+{
+  draw_apple=loadImage("apple.png");
+}
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
